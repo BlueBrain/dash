26afdf0789297ea5b40b44034a1a3848650208dc
Stefan Eilemann <stefan.eilemann@epfl.ch>
2012-02-29 17:27:42 +0100

    Copyright notices

--- tests/pipeline/pipelineMT.cpp
+++ tests/pipeline/pipelineMT.cpp
@@ -1,6 +1,22 @@
 
-// Copyright (c) 2011 EPFL/BBP. All rights reserved.
-// Author: Ahmet Bilgili
+/* Copyright (c) 2011, EFPL/Blue Brain Project
+ *                     Ahmet Bilgili
+ *
+ * This file is part of DASH <https://github.com/BlueBrain/dash>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include "test.h"
 
