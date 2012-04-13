f16eed645e34a21ae8ece360e01e6ddd7f11ff2e
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-04-13 14:35:22 +0200

    Any serialization support for lunchbox::uint128_t

--- tests/attribute/serialize.cpp
+++ tests/attribute/serialize.cpp
@@ -30,19 +30,25 @@ int dash::test::main( int argc, char **argv )
 
     dash::AttributePtr intAttribute = new dash::Attribute( 42 );
     textSerialize( *intAttribute, *outAttribute );
-    TEST( intAttribute->get<int>() == outAttribute->get<int>() )
+    TEST( intAttribute->get<int>() == outAttribute->get<int>( ))
     binarySerialize( *intAttribute, *outAttribute );
-    TEST( intAttribute->get<int>() == outAttribute->get<int>() )
+    TEST( intAttribute->get<int>() == outAttribute->get<int>( ))
 
     dash::AttributePtr floatAttribute = new dash::Attribute( 42.f );
     textSerialize( *floatAttribute, *outAttribute );
-    TEST( floatAttribute->get<float>() == outAttribute->get<float>() )
+    TEST( floatAttribute->get<float>() == outAttribute->get<float>( ))
 
     dash::AttributePtr stringAttribute =
-                                     new dash::Attribute( std::string( "bla") );
+                                     new dash::Attribute( std::string( "bla" ));
     textSerialize( *stringAttribute, *outAttribute );
     TEST( stringAttribute->get<std::string>() ==
-          outAttribute->get<std::string>() )
+          outAttribute->get<std::string>( ))
+
+    dash::AttributePtr uint128Attribute =
+                      new dash::Attribute( lunchbox::uint128_t( 12345, 54321 ));
+    textSerialize( *uint128Attribute, *outAttribute );
+    TEST( uint128Attribute->get<lunchbox::uint128_t>() ==
+          outAttribute->get<lunchbox::uint128_t>( ))
 
     Foo foo = {42, 1.5f, false, "blablub"};
     dash::AttributePtr fooAttribute = new dash::Attribute( foo );
