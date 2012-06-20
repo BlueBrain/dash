a9414778b0f6708a53818bad16fdedc59552800f
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-06-20 16:51:16 +0200

    Unit test for #3

---
+++ tests/attribute/readwrite.cpp
@@ -0,0 +1,52 @@
+
+/* Copyright (c) 2011-2012, EFPL/Blue Brain Project
+ *                     Stefan Eilemann <stefan.eilemann@epfl.ch> 
+ *
+ * This file is part of DASH <https://github.com/BlueBrain/dash>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "test.h"
+
+#include <dash/context.h>
+#include <dash/attribute.h>
+
+int dash::test::main( int argc, char **argv )
+{
+    dash::Context& mainCtx = dash::Context::getMain( argc, argv );
+
+    dash::AttributePtr attr1 = new dash::Attribute( 42 );
+
+    {
+        dash::Context auxCtx;
+        mainCtx.map( attr1, auxCtx );
+
+        TEST( attr1->get< int >() == 42 );
+
+        auxCtx.setCurrent();
+        *attr1 = 17;
+        TEST( attr1->get< int >() == 17 );
+
+        mainCtx.setCurrent();
+        TEST( attr1->get< int >() == 42 );
+
+        auxCtx.setCurrent();
+        mainCtx.apply( auxCtx.commit( ));
+        mainCtx.setCurrent();
+        TEST( attr1->get< int >() == 17 );
+    }
+
+    return EXIT_SUCCESS;
+}
